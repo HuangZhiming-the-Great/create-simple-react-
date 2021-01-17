6c225ac8cd73871c887f4ee483f6bbd3530a979d
HuangZhiming-the-Great <740616790@qq.com>
2021-01-17 18:30:56 +0800

    init.

---
+++ README.md
@@ -0,0 +1,10 @@
+## 这个是一个网上自学的项目
+- 网上资料：
+  [一篇自己搭建react脚手架的文章](https://blog.csdn.net/design_lu/article/details/94856399)
+## 原教程里有的问题
+1. babel编译报错
+  
+   [这篇文章](https://blog.csdn.net/weixin_41779718/article/details/105149338)解决了问题。
+2. webpack编译遇到问题
+   
+   这个[总结性文章](cnblogs.com/jeacy/p/13864454.html)可以很好的解决问题。
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path=require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+
+module.exports = {
+  mode: 'development',
+  entry: './src/foo.js',
+  output: {
+    path: path.resolve(__dirname,'dist'),
+    filename: 'foo.bundle.js'
+  },
+  plugins:[
+    new HtmlWebpackPlugin({ template: './index.html'}),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/react","@babel/env"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname,'dist'),
+    compress: true,
+    port: 9000
+  }
+}
